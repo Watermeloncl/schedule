5415de997f4392509f74c649f4d046327bab4fc2
Watermeloncl <devinfrandsen@gmail.com>
2018-07-19 09:58:54 -0600

    addedtwo hours of course

--- src/components/action.js
+++ src/components/action.js
@@ -4,8 +4,8 @@ import Icon from './icon';
 class Action extends Component {
     render() {
         return (
-            <button onClick={() => this.props.onClick} className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
+            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+                { Icon("fas fa-plus-circle") }
                 {/* fas fa-times-circle */}
             </button>
         )
